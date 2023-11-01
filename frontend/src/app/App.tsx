08a309907dfe68c3503cd94c0e1b11eae1f42471
islam haluhaev <islamhaluhaev225@gmail.com>
2023-11-01 14:23:15 +0300

    hr

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Input } from '@/shared/ui/Input/Input'
 import { Form } from '../shared/ui/index'
+import { Hr } from '@/shared/ui/Hr/Hr'
 
 const App = () => {
   return (
     <div className='app app_light'>
-
+      <Hr /> <span>rrrrr</span>
     </div>
   )
 }
