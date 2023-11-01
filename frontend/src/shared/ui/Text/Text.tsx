b2f150f7d23f2439071fdcda33f6449601890ab8
katsiev1997 <105316523+katsiev1997@users.noreply.github.com>
2023-11-01 10:43:02 +0300

    Text fix

--- frontend/src/shared/ui/Text/Text.tsx
+++ frontend/src/shared/ui/Text/Text.tsx
@@ -1,19 +1,20 @@
 import { FC } from 'react'
 import cls from './Text.module.scss'
+import { classNames } from '@/shared/lib/classNames'
 
 export type TextWeight = 200 | 400 | 500 | 600 | 700
 export type TextSize = 12 | 14 | 18 | 22 | 28
 export type TextColor = 'blue' | 'black' | 'gray'
-export type TextTag = "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "span" | "p"
+export type TextTag = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span' | 'p'
 
-const sizeClasess: Record<TextSize, string> = {
+const sizeClasses: Record<TextSize, string> = {
   12: cls.size12,
   14: cls.size14,
   18: cls.size18,
   22: cls.size22,
   28: cls.size28,
 }
-const weightClasess: Record<TextWeight, string> = {
+const weightClasses: Record<TextWeight, string> = {
   200: cls.weight200,
   400: cls.weight400,
   500: cls.weight500,
@@ -28,6 +29,7 @@ const colorClasses: Record<TextColor, string> = {
 }
 
 interface TextProps {
+  className?: string
   children: string
   size: TextSize
   weight: TextWeight
@@ -36,7 +38,13 @@ interface TextProps {
 }
 
 export const Text: FC<TextProps> = (props) => {
-  const { children, size, weight, color, tag } = props
+  const { className, children, size, weight, color, tag } = props
+  const classes = [
+    size && sizeClasses[size],
+    weight && weightClasses[weight],
+    color && colorClasses[color],
+    className,
+  ]
   const Tag = tag
-  return <Tag>{children}</Tag>
+  return <Tag className={classNames('', {}, classes)}>{children}</Tag>
 }
